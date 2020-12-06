1318fb53d252c94d8bd7949b3f581a1f540ca6d1
dante <c.dante.federici@gmail.com>
2020-12-06 00:38:08 -0500

    Better skeleton

---
+++ src/day-00/index.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import fp from 'lodash/fp.js';
+import * as util from '../util.js';
+
+export const part1 = (input) => {
+	return undefined;
+};
+
+export const part2 = (input) => {
+	return undefined;
+};
+
+const main = () => {
+	const input = fs.readFileSync('./input.txt').toString();
+	console.log('Part 1', part1(input));
+	console.log('Part 2', part2(input));
+};
+// main();
