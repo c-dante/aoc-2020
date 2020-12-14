13956be741f472fd99b5012f8fd7c083cab55611
dante <c.dante.federici@gmail.com>
2020-12-14 03:31:00 -0500

    Wow I'm dumb

--- src/day-14/index.js
+++ src/day-14/index.js
@@ -1,34 +1,38 @@
 import fs from 'fs';
 import fp from 'lodash/fp.js';
-import BitSet from 'bitset';
 import { CartesianProduct } from 'js-combinatorics';
+
+const ZERO = BigInt(0);
+const ONE = BigInt(1);
+const MAX = BigInt("0b111111111111111111111111111111111111");
 
 export const part1 = (input) => {
 	const res = input.split('\n').reduce((acc, x) => {
 		const [l, op, r] = x.split(' ');
 		const [_, name, __, idx] = /(\w+)(\[(\w+)\])?/.exec(l);
 		if (name === 'mask') {
-			acc.mask = [];
+			acc.maskOn = ZERO;
+			acc.maskOff = MAX;
 			r.split('')
 				.forEach((x, i) => {
 					if (x === '1') {
-						acc.mask.push([35 - i, 1]);
+						acc.maskOn |= ONE << BigInt(35 - i);
 					} else if (x === '0') {
-						acc.mask.push([35 - i, 0]);
+						acc.maskOff ^= ONE << BigInt(35 - i);
 					}
 				});
 		} else {
-			const val = new BitSet(+r);
-			acc.mask.forEach(([bit, state]) => val.set(bit, state));
-			acc.mem[+idx] = +val.toString(10);
+			const val = ((BigInt(+r) | acc.maskOn) & acc.maskOff);
+			acc.mem.set(+idx, Number(val));
 		}
 		return acc;
 	}, {
-		mem: [],
-		mask: [],
+		mem: new Map(),
+		maskOn: ZERO,
+		maskOff: MAX,
 	});
 
-	return fp.sum(res.mem);
+	return fp.sum([...res.mem.values()]);
 };
 
 
@@ -37,47 +41,50 @@ export const part2 = (input) => {
 		const [l, op, r] = x.split(' ');
 		const [_, name, __, idx] = /(\w+)(\[(\w+)\])?/.exec(l);
 		if (name === 'mask') {
-			acc.base = [];
+			acc.baseOn = ZERO;
 			const floating = [];
 			r.split('')
 				.forEach((x, i) => {
 					if (x === '1') {
-						acc.base.push(35 - i);
+						acc.baseOn |= ONE << BigInt(35 - i);
 					} else if (x === 'X') {
 						floating.push([
-							[35 - i, 0],
-							[35 - i, 1],
+							[BigInt(35 - i), ZERO],
+							[BigInt(35 - i), ONE],
 						]);
 					}
 				});
 				acc.set = new CartesianProduct(...floating);
 		} else {
-			const mem = new BitSet(+idx);
-			acc.base.forEach((bit) => {
-				mem.set(bit, 1);
-			});
+			const withOn = BigInt(+idx) | acc.baseOn;
+
 			for (const perm of acc.set) {
-				const newBase = mem.clone();
-				perm.forEach(([bit, state]) => newBase.set(bit, state));
-				acc.mem[+newBase.toString(10)] = +r;
+				const newBase = perm.reduce((acc, [bit, state]) => {
+					if (state) {
+						return acc |= ONE << bit;
+					}
+
+					return acc &= MAX ^ (ONE << bit);
+				}, withOn);
+				acc.mem.set(Number(newBase), +r);
 			}
 		}
 		return acc;
 	}, {
-		mem: [],
-		base: [],
+		mem: new Map(),
+		baseOn: ZERO,
 		set: undefined
 	});
 
-	return fp.sum(res.mem);
+	return fp.sum([...res.mem.values()]);
 };
 
 
 const main = () => {
 	const input = fs.readFileSync('./input.txt').toString();
-	// console.time('part 1');
+	console.time('part 1');
 	console.log('Part 1', part1(input));
-	// console.timeEnd('part 1');
+	console.timeEnd('part 1');
 	console.time('part 2');
 	console.log('Part 2', part2(input));
 	console.timeEnd('part 2');
