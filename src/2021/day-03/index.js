2542e271d06abcabaaf137e7265833bcb597a64e
dante <c.dante.federici@gmail.com>
2021-12-03 23:58:36 -0500

    Day 3 2021

--- src/2021/day-03/index.js
+++ src/2021/day-03/index.js
@@ -4,26 +4,67 @@ import * as util from '../../util.js';
 
 const process = fp.flow(
 	fp.split('\n'),
-	fp.map(x => x)
+	fp.map(x => x.split('').map(x => +x)),
+	fp.reduce((acc, x) => {
+		x.forEach((z, i) => {
+			if (!acc[i]) {
+				acc[i] = [0, 0];
+			}
+			acc[i][0] += z === 0 ? 1 : 0;
+			acc[i][1] += z === 1 ? 1 : 0;
+		});
+		return acc;
+	}, []),
+	x => {
+		return [
+			x.map(z => z[0] > z[1] ? 0 : 1),
+			x.map(z => z[0] > z[1] ? 1 : 0),
+		]
+	},
 );
 
 export const part1 = (input) => {
 	const res = process(input)
-		.reduce((acc, x) => {
-			return acc;
-		}, {
-
-		});
-	return res;
+	return parseInt(res[0].join(''), 2) * parseInt(res[1].join(''), 2);
 };
 
-export const part2 = (input) => {
-	const res = process(input)
-		.reduce((acc, x) => {
-			return acc;
-		}, {
+const process2 = fp.flow(
+	fp.split('\n'),
+	fp.map(x => x.split('').map(x => +x)),
+	data => {
+		let idx = 0;
+		let most = data.slice();
+		while (most.length > 1) {
+			const bits = most.reduce((acc, x) => {
+				acc[0] += x[idx] === 0 ? 1 : 0;
+				acc[1] += x[idx] === 1 ? 1 : 0;
+				return acc;
+			}, [0, 0]);
+			const bit = bits[0] > bits[1] ? 0 : 1;
+			most = most.filter(x => x[idx] === bit);
+			idx++;
+		}
+		const a = parseInt(most[0].join(''), 2);
 
-		});
+		idx = 0;
+		let least = data.slice();
+		while (least.length > 1) {
+			const bits = least.reduce((acc, x) => {
+				acc[0] += x[idx] === 0 ? 1 : 0;
+				acc[1] += x[idx] === 1 ? 1 : 0;
+				return acc;
+			}, [0, 0]);
+			const bit = bits[0] > bits[1] ? 1 : 0;
+			least = least.filter(x => x[idx] === bit);
+			idx++;
+		}
+		const b = parseInt(least[0].join(''), 2);
+		return a * b;
+	}
+);
+
+export const part2 = (input) => {
+	const res = process2(input)
 	return res;
 };
 
--- src/2021/day-04/test.js
+++ src/2021/day-04/test.js
@@ -1,18 +1,6 @@
 import { part1, part2 } from './index.js';
 
-const input = `00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010
-`;
+const input = ``;
 
 console.log(
 	'Part 1 (T)',
@@ -23,5 +11,5 @@ console.log(
 console.log(
 	'Part 2 (T)',
 	part2(input),
-	230
+	'?'
 );
