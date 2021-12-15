9b5097b294a05118b3ff9aac2a96a266436d7cc3
dante <c.dante.federici@gmail.com>
2021-12-15 01:10:20 -0500

    Better with just a min heap and math

--- src/2021/day-15/index.js
+++ src/2021/day-15/index.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import fp from 'lodash/fp.js';
 import Graph from 'node-dijkstra';
+import Heap from 'heap';
 import * as util from '../../util.js';
 
 const process = fp.flow(
@@ -10,79 +11,67 @@ const process = fp.flow(
 
 const key = pt => pt.join(',');
 
+const getGridValue = (grid, x, y) => {
+	const w = grid[0].length;
+	const h = grid.length;
+	const mx = Math.floor(x / w);
+	const my = Math.floor(y / h);
+	return ((grid[y % h][x % w] - 1) + mx + my) % 9 + 1;
+}
+
+const gridSearch = (grid, w, h) => {
+	const heap = new Heap((a, b) => {
+		return a.cost - b.cost;
+	});
+
+	heap.push({ cost: 0, path: [[0, 0]] })
+	let searching = true;
+	const visited = new Set(key([0, 0]));
+	const END = key([w - 1, h - 1])
+	while (searching && !heap.empty()) {
+		const best = heap.pop();
+		const lastStep = fp.last(best.path);
+		if (key(lastStep) === END) {
+			searching = false;
+			// console.log(best);
+			return best.cost;
+		}
+
+		// Add next steps to the heap
+		util.cardinal.forEach(([dx, dy]) => {
+			const nx = lastStep[0] + dx;
+			const ny = lastStep[1] + dy;
+			const next = [nx, ny];
+			const nextKey = key(next);
+			if (visited.has(nextKey) || nx < 0 || ny < 0 || nx >= w || ny >= h) return;
+			const path = best.path.concat([next]);
+			visited.add(nextKey);
+			heap.push({
+				path,
+				cost: best.cost + getGridValue(grid, nx, ny),
+			});
+		});
+	}
+}
+
 export const part1 = (input) => {
 	const grid = process(input);
-	const h = grid.length;
-	const w = grid[0].length;
-
-	const graph = new Graph();
-	grid.forEach((row, y) => row.forEach((cell, x) => {
-		const edges = util.cardinal.reduce((acc, [dx, dy]) => {
-			const nx = x + dx;
-			const ny = y + dy;
-			if (nx < 0 || ny < 0 || nx >= w || ny >= h) return acc;
-			acc[key([nx, ny])] = grid[ny][nx];
-			return acc;
-		}, {});
-		graph.addNode(key([x, y]), edges);
-	}))
-
-	const path = graph.path(key([0, 0]), key([w - 1, h - 1]));
-	return path.slice(1).map(a => {
-		const [x, y] = a.split(',').map(Number);
-		return grid[+y][+x];
-	}).reduce((a, b) => a + b);
+	return gridSearch(grid, grid[0].length, grid.length);
 };
 
 export const part2 = (input) => {
 	const grid = process(input);
-	const bh = grid.length;
-	const bw = grid[0].length;
-	for (let i = 0; i < 4; i++) {
-		grid.push(
-			...grid.slice(i * bh, (i + 1) * bh)
-				.map(row => row.map(x => (x % 9) + 1))
-		);
-	}
-	for (let i = 0; i < 4; i++) {
-		grid.forEach(row => {
-			row.push(
-				...row.slice(i * bw, (i + 1) * bw)
-					.map(x => (x % 9) + 1)
-			);
-		});
-	}
-	const h = grid.length;
-	const w = grid[0].length;
-
-
-	const graph = new Graph();
-	grid.forEach((row, y) => row.forEach((cell, x) => {
-		const edges = util.cardinal.reduce((acc, [dx, dy]) => {
-			const nx = x + dx;
-			const ny = y + dy;
-			if (nx < 0 || ny < 0 || nx >= w || ny >= h) return acc;
-			acc[key([nx, ny])] = grid[ny][nx];
-			return acc;
-		}, {});
-		graph.addNode(key([x, y]), edges);
-	}))
-
-	const path = graph.path(key([0, 0]), key([w - 1, h - 1]));
-	return path.slice(1).map(a => {
-		const [x, y] = a.split(',').map(Number);
-		return grid[+y][+x];
-	}).reduce((a, b) => a + b);
+	return gridSearch(grid, grid[0].length * 5, grid.length * 5);
 };
 
 
 const main = () => {
 	const input = fs.readFileSync('./input.txt').toString();
-	// console.time('part 1');
+	console.time('part 1');
 	console.log('Part 1', part1(input));
-	// console.timeEnd('part 1');
-	// console.time('part 2');
+	console.timeEnd('part 1');
+	console.time('part 2');
 	console.log('Part 2', part2(input));
-	// console.timeEnd('part 2');
+	console.timeEnd('part 2');
 };
 main();
